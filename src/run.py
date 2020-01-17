d5eacdd26845f5ab05033c271cb46d905be25287
Calvin Choy <38222658+choykalun@users.noreply.github.com>
2020-01-17 16:55:04 +0000

    Add user login prototype in run.py

--- src/run.py
+++ src/run.py
@@ -1,15 +1,27 @@
+import quart.flask_patch
+import flask_login
+
 from quart import Quart
-from quart import session
+from quart import session, request
+
+
 
 app = Quart(__name__)
+app.secret_key = 'mysecret'
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 async def index():
     return "Hello World!"
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 async def login():
-	return "success"
+	args = request.headers
+	# print(args["Username"])
+	# print(args["Password"])
+	if args["Username"] == "" or args["Password"] == "" :
+		return '{"message" : "error! username or password are incorrect"}', 404
 
-@app.route("/comic", method=["GET", "POST"])
+	return args
