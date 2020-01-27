aba64d8a400a9d2a6867bcc3d62d252c8fbe162f
Calvin Choy <38222658+choykalun@users.noreply.github.com>
2020-01-27 18:00:43 +0000

    remove sqlalchemy because it does not work & fix create user function

--- src/run.py
+++ src/run.py
@@ -1,37 +1,20 @@
 import quart.flask_patch
-import flask_login
 
 from quart import Quart
-from quart import session, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from quart import session, request, jsonify, g
 from pathlib import Path
 from sqlite3 import dbapi2 as sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Quart(__name__)
 app.secret_key = 'mysecret'
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
 app.config.update({'DATABASE' : app.root_path/'api_server_comic.db'})
-app.config['SQLALCHEMY_DATABASE_URI'] = app.root_path/'api_server_comic.db'
-db = SQLAlchemy(app)
 
-
-class User(db.Model):
-	__tablename__ = 'Users'
-	userid = db.Column('userid', db.Integer, primary_key=True, unique=True)
-	first_name = db.Column('firstname', db.String(20))
-	last_name = db.Column('lastname', db.String(20))
-	email = db.Column('email', db.String(50), unique=True)
-	password = db.Column('password', db.String(80))
-	username = db.Column('username', db.String(50), unique=True)
-
-	def __init__(self, username, firstname, lastname, email, password):
-		self.first_name = firstname
-		self.last_name = lastname
-		self.email = email
-		self.password = password
-		self.username = username
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
 
 def connect_db():
 	engine = sqlite3.connect(app.config['DATABASE'])
@@ -51,22 +34,29 @@ async def index():
 
 @app.route("/user", methods=['POST'])
 async def create_user():
-	db = connect_db()
-	data = request.get_json();
+	db = get_db()
+	data = await request.get_json()
+	cur = db.execute("""SELECT * FROM Users WHERE email=?""", [data["email"]])
+	exist = cur.fetchall()
+
+	if len(exist) > 0:
+		return jsonify({"message" : "This email already exists"}), 409
+	else:
+		hashed_password = generate_password_hash(data["password"], method="sha256")
+		cur = db.execute("""INSERT INTO Users (firstname, lastname, email, password, username) VALUES (?, ?, ?, ?, ?)""", [data["firstname"], data["lastname"], 
+			data["email"], hashed_password, data["username"]])
+		db.commit()
 
 	print(data["firstname"])
 
-	new_user = User(firstname=data["firstname"], lastname=data["lastname"], email=data["email"], password=data["password"], username=data["username"])
-	print(new_user)
-	db.session.add(new_user)
-	db.session.commit()
 
 	return jsonify({"message" : "User has been created!"})
 
 
 @app.route("/login", methods=['POST'])
 async def login():
-	args = request.headers
+	db = get_db()
+	data = await request.get_json()
 	# print(args["Username"])
 	# print(args["Password"])
 	if args["Username"] == "" or args["Password"] == "" :
